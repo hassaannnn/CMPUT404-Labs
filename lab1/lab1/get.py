8bb48d423baee2b054d54352b370b5687498ac58
hass <hassaan3@ualberta.ca>
2022-09-12 18:12:44 -0600

    script update

--- lab1/lab1/get.py
+++ lab1/lab1/get.py
@@ -1,5 +1,8 @@
 import requests as r
 
 print(r.__version__)
+#Getting google
+
 x = r.get('http://google.com')
-print(x)
+myownscript = r.get("https://raw.githubusercontent.com/hassaannnn/CMPUT401-Labs/master/lab1/lab1/get.py").content
+print(myownscript)
