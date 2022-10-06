981f032d546c549df529d55dc7f1ea7328f48523
hass <hassaan3@ualberta.ca>
2022-10-05 18:33:29 -0600

    lab 4

---
+++ lab4/mysite/polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('api/questions/', views.get_questions, name='get_questions'),
+    path('api/question/<int:pk>', views.update_question, name='update_question'),
+
+]
